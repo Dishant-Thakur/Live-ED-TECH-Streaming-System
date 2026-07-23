a6d15e418704d91b137cece32c06afc984ba03c6
Dishant Thakur <dishantthakur029@gmail.com>
2026-07-23 20:46:41 +0530

    update in responsive design

--- src/server.js
+++ src/server.js
@@ -3,14 +3,12 @@ const path = require("path");
 require("dotenv").config();
 
 const helmet = require("helmet");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
@@ -25,10 +23,18 @@ app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "register.html"));
 });
 
-app.use((req, res) => {
-    res.status(404).send("404 - Page Not Found");
-});
+app.post("/",(req,res)=>{
+    const {name, number, email, course, timing, mode, qualification, status, extraData} = req.body;
+    if(!req.body){
+        return res.send('Sorry no data');
+    }
+    console.log(req.body);
+    res.send('Data send');
+})
 
+app.use((req, res) => {
+    res.status(404).send("HTTP ERROR 404 - Page Not Found");
+});
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
